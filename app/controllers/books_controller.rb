41ae15d07a250b0d25146e46dcd4707cb2f10d65
EC2 Default User <ec2-user@ip-172-31-47-102.ap-northeast-1.compute.internal>
2022-12-22 18:12:59 +0000

    20221223

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,65 @@
 class BooksController < ApplicationController
-  
+
+
+
+
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @post.save
-    redirect_to post_images_path
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
-  
+
 
   def index
+      @user = current_user
+      @book = Book.new
+      @books = Book.all
+     
   end
 
   def show
+    	@book = Book.find(params[:id])
+      @user = User.find(@book.user_id)
+    	@booknew = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+    def update
+       @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render :edit
+    end
+    end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
 
   private
 
-  def post_image_params
-    params.require(:books).permit(:shop_name, :image, :caption)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,35 @@
 class UsersController < ApplicationController
- 
+  
+  
+  
   def show
+    @book = Book.new
     @user = User.find(params[:id])
-    @post_images = @user.post_images.page(params[:page])
+    @books = @user.books
   end
 
   
   def edit
      @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  def index
+      @user = current_user
+      @users = User.all
+      @book = Book.new
+  end
+  
 
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
+    end
+    
 end
